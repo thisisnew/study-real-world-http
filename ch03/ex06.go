5925df5390749e1e75e28910c4acfe5697d17674
thisisnew <thisisnew@naver.com>
2023-01-03 15:18:17 +0900

    study real wolrd http chapter03 ex06 header

---
+++ ch03/ex06.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Head("http://localhost:18888")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Status:", resp.Status)
+	log.Println("Header:", resp.Header)
+	log.Println("Body:", resp.Body)
+}
